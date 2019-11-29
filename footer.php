531a036647971cab776245708e5d5d83b1c72bf9
nguyenthanhtuong <thanhtuongpro776@gmail.com>
2019-11-29 10:37:37 +0700

    day la cac trang ben nguoi dung v1.0

---
+++ footer.php
@@ -0,0 +1,25 @@
+<div id="footer">
+  <div class="container">
+    <div class="desc">
+      <p><a href="javascript:void();" title="webphim">TheWall</a> - <a href="javascript:void();" title="Xem phim online"><strong>Xem trailer online</strong></a> đặt vé miễn phí, chất lượng hình ảnh full HD không che, tốc độ tải tương đương the flash, <a href="javascript:void();"
+          title="Xem phim"><strong>xem trailer phim</strong></a> không phải chờ đợi lâu. TheWal luôn cập nhật <a title="Phim mới" href="javascript:void();"><strong>phim gần mới</strong></a> để mang đến cho các bạn những bộ <a title="Phim hành động"
+          href="javascript:void();"><strong>phim hành động</strong></a>, võ thuật, <a title="Phim chiếu rạp" href="javascript:void();"><strong>phim đang chiếu rạp</strong></a>, các thể loại phim tâm lý, tình
+        cảm cực lôi cuốn và hấp dẫn nhất. Đặc biệt website rất thân thiện với người dùng và không có các quảng cáo gây khó chịu khi <strong>xem phim bởi vì đang chạy trên localhost mà :v</strong>. Chúc các bạn <strong>xem trailer</strong> vui vẻ, à và nhớ books vé ủng hộ tui.</p>
+    </div>
+    <div id="info">
+      <div class="column">
+        <div class="heading">Liên hệ</div>
+        <ul>
+          <li><a href="javascript:void();">Liên hệ quảng cáo</a></li>
+          <li><a href="javascript:void();">TheWall</a></li>
+          <li><a href="javascript:void();">0347960246</a></li>
+        </ul>
+      </div>
+      <div class="column">
+        <div class="heading">Điều khoản sử dụng</div>
+        <ul>
+          <li><a href="javascript:void();">ai xin thì cho thôi</a></li>
+          <li><a href="javascript:void();">nói chứ cũng tùy người :v</a></li>
+        </ul>
+  </div>
+</div>
