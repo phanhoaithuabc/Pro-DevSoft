18d4014fe5e5a459669a7be6fca089380df20907
nguyenthanhtuong <thanhtuongpro776@gmail.com>
2019-12-02 10:11:58 +0700

    v1.1

--- Index.php
+++ Index.php
@@ -21,24 +21,11 @@
     if($mod==''){
       $mod='home';
     }
-    elseif($mod=='list') {
-      echo '<link href="css/style_listfilm.css" type="text/css" rel="stylesheet">';
-    }
     elseif($mod=='detail'){
       echo '<link href="css/style-detail.css" type="text/css" rel="stylesheet">';
     }
     elseif($mod=='booking'){
       echo '<link href="css/style-detail.css" type="text/css" rel="stylesheet">';
-    }
-    elseif($mod=='bookchair'){
-      echo '<link href="css/style-detail.css" type="text/css" rel="stylesheet">';
-    }
-    elseif ($mod=='watch'){
-      echo '<link rel="stylesheet" type="text/css" media="screen" href="css/style_watch.css" />';
-      echo '<script src="js/watch.js"></script>';
-      echo '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">';
-      echo '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>';
-      echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>';
     }
   ?>
   <script src="js/jquery.min.js" type="text/javascript"></script>
